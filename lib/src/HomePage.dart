c4485e7aca62064f7cc66ee0ea9e2b5a986a0d59
Kuriioo <61805275+Kuriioo@users.noreply.github.com>
2020-09-27 18:44:20 +0200

    Add files via upload

--- lib/src/HomePage.dart
+++ lib/src/HomePage.dart
@@ -1,17 +1,46 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      child: Text(
-        "Welcome to HexaCode",
-        style: TextStyle(
-          fontSize: 24.0,
-          fontWeight: FontWeight.bold,
-        ),
+      body: Column(
+        children: [
+          ButtonBar(
+            alignment: MainAxisAlignment.start,
+            buttonMinWidth: 50.0,
+            children: [
+              FlatButton(
+                child: Text('Close'),
+                color: Colors.blue,
+                onPressed: () => {exit(0)},
+              ),
+            ],
+          ),
+          Expanded(
+            flex: 10,
+            child: TextField(
+              showCursor: true,
+              style: TextStyle(
+                fontSize: 26.0,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: "",
+                labelStyle: TextStyle(
+                  fontSize: 26.0,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
-    ));
+    );
   }
 }
